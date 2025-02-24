045e6dc9c3c0dcf764757be6cef7b1b5030611db
mayur-driod <settymayurk@gmail.com>
2025-02-24 14:52:25 +0530

    done

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+require('dotenv').config();
+const router = express.Router();
+// const users = require('./data.json');
+// console.log(users);
+
+const users = [{username:"Alice", age:25, email:"alice@gmail.com"}];
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+
+router.get('/find', (req,res)=>{
+    const {username} = req.query;
+    if(username == ''){
+        return res.status(400).json({message:"query cannot be empty!"})
+    }
+    try{
+        const userexist = users.find((ele) => ele.username == username);
+        console.log(userexist);
+        if(!userexist){
+            return res.status(400).json({message:"User Not Found"});
+        }
+        res.status(200).json({message:"The user was found", data: userexist});
+    }
+    catch(err){
+
+    }
+});
+
+app.use('/',router)
+
+app.listen(port, ()=>{
+    console.log(`server running at http://localhost:${port}`);
+});
